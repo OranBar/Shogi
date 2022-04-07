913accc41cae30bcad45ba7605bb67f2aee60bfe
Oran Bar (Fisso) <oranbar@gmail.com>
2022-04-08 00:46:33 +0200

    We're serializing interfaces baby

--- Assets/__Scripts/Test/PieceTest.cs
+++ Assets/__Scripts/Test/PieceTest.cs
@@ -2,24 +2,40 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace Shogi {
 	public class PieceTest : MonoBehaviour {
 		public Piece piece;
-		//We're doing depencenty injecton by referencing MB from inspector
-		[SerializeReference] public IMovementStrategy defaultMovement;
-		public IMovementStrategy promotedMovement;
+		//We're doing depencenty injection by referencing MB from inspector
+		[SerializeField, InterfaceType( typeof( IMovementStrategy ) )] 
+		private Object _defaultMovement;
+		public IMovementStrategy DefaultMovement
+		{
+			get => _defaultMovement as IMovementStrategy;
+			set => _defaultMovement = (Object)value;
+		}
+
+		[SerializeField, InterfaceType( typeof( IMovementStrategy ) )]
+		private Object _promotedMovement;
+		public IMovementStrategy PromotedMovement
+		{
+			get => _promotedMovement as IMovementStrategy;
+			set => _promotedMovement = (Object)value;
+		}
+		
 
 		public int x, y;
 
 		private BoardTest board;
 		
+
 		void Awake(){
 			board = FindObjectOfType<BoardTest>();
 		}
 
 		void Start() {
-			piece = new Piece( board.board, x, y, defaultMovement );
+			piece = new Piece( board.board, x, y, DefaultMovement );
 		}
 
 		public void PreviewAvailableMoves(){
@@ -27,7 +43,7 @@ namespace Shogi {
 		}
 
 		public void Promote(){
-			piece.movementStrategy = promotedMovement;
+			piece.movementStrategy = PromotedMovement;
 		}
 	}
 }
