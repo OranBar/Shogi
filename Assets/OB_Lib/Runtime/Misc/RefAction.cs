16928ed6d2c78e2eda050481d824cfe957cfb2d4
Oran Bar (Laptop) <oran.bar@softwareengineering.it>
2022-04-16 01:27:25 +0200

    RefAction refactor to OBLib

---
+++ Assets/OB_Lib/Runtime/Misc/RefAction.cs
@@ -0,0 +1,84 @@
+
+using System;
+
+public class RefAction
+{
+	public Action Value { get; set; } = () => { };
+
+	public void SubscribeOnce( RefAction a, Action toSubscribe ) {
+		Action invokeThenUnsubscribe = null;
+		invokeThenUnsubscribe = () =>
+		{
+			toSubscribe.Invoke();
+			a.Value -= invokeThenUnsubscribe;
+		};
+
+		a.Value += invokeThenUnsubscribe;
+	}
+
+}
+
+public class RefAction<T>
+{
+	public Action<T> Value { get; set; } = ( _ ) => { };
+
+	public void SubscribeOnce<T>( RefAction<T> a, Action<T> toSubscribe ) {
+		Action<T> invokeThenUnsubscribe = null;
+		invokeThenUnsubscribe = ( arg ) =>
+		{
+			toSubscribe.Invoke( arg );
+			a.Value -= invokeThenUnsubscribe;
+		};
+
+		a.Value += invokeThenUnsubscribe;
+	}
+
+}
+
+public class RefAction<T, U>
+{
+	public Action<T, U> Value { get; set; } = ( _1, _2 ) => { };
+
+	public void SubscribeOnce<T, U>( RefAction<T, U> a, Action<T, U> toSubscribe ) {
+		Action<T, U> invokeThenUnsubscribe = null;
+		invokeThenUnsubscribe = ( arg1, arg2 ) =>
+		{
+			toSubscribe.Invoke( arg1, arg2 );
+			a.Value -= invokeThenUnsubscribe;
+		};
+
+		a.Value += invokeThenUnsubscribe;
+	}
+}
+
+public class RefAction<T, U, V>
+{
+	public Action<T, U, V> Value { get; set; } = ( _1, _2, _3 ) => { };
+
+	public void SubscribeOnce<T, U, V>( RefAction<T, U, V> a, Action<T, U, V> toSubscribe ) {
+		Action<T, U, V> invokeThenUnsubscribe = null;
+		invokeThenUnsubscribe = ( arg1, arg2, arg3 ) =>
+		{
+			toSubscribe.Invoke( arg1, arg2, arg3 );
+			a.Value -= invokeThenUnsubscribe;
+		};
+
+		a.Value += invokeThenUnsubscribe;
+	}
+}
+
+public class RefAction<T, U, V, Z>
+{
+	public Action<T, U, V, Z> Value { get; set; } = ( _1, _2, _3, _4 ) => { };
+
+	public void SubscribeOnce<T, U, V, Z>( RefAction<T, U, V, Z> a, Action<T, U, V, Z> toSubscribe ) {
+		Action<T, U, V, Z> invokeThenUnsubscribe = null;
+		invokeThenUnsubscribe = ( arg1, arg2, arg3, arg4 ) =>
+		{
+			toSubscribe.Invoke( arg1, arg2, arg3, arg4 );
+			a.Value -= invokeThenUnsubscribe;
+		};
+
+		a.Value += invokeThenUnsubscribe;
+	}
+}
