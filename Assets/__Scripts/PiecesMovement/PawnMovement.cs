3039cdcbda1f5722e4e5c24347594c0cfe4e4d4d
Oran Bar (Laptop) <oran.bar@softwareengineering.it>
2022-04-16 14:16:57 +0200

    Improved movement to take into account piece owner

--- Assets/__Scripts/PiecesMovement/PawnMovement.cs
+++ Assets/__Scripts/PiecesMovement/PawnMovement.cs
@@ -10,18 +10,11 @@ namespace Shogi
 		}
 
 		public List<(int x, int y)> GetAvailableMoves( int startX, int startY ) {
+			int direction = piece.OwnerId == PlayerId.Player1 ? 1 : -1;
+
 			List<(int x, int y)> result = new List<(int x, int y)>();
-			result.Add( GetCellIndex_Forward( startX, startY, 1 ) );
+			result.Add( (startX, startY + ( 1 * direction )) );
 			return result;
-		}
-
-
-		private (int x, int y) GetCellIndex_Forward( int startX, int startY, int distance ) {
-			if (piece.OwnerId == PlayerId.Player1) {
-				return (startX, startY + distance);
-			} else {
-				return (startX, startY - distance);
-			}
 		}
 	}
 
