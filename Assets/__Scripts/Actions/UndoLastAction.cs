7d4832c06bd1dd99e0d74701c058ceeb8dcd1a1d
Oran Bar (Fisso) <oranbar@gmail.com>
2022-05-07 20:19:21 +0200

    bugfix

--- Assets/__Scripts/Actions/UndoLastAction.cs
+++ Assets/__Scripts/Actions/UndoLastAction.cs
@@ -11,13 +11,13 @@ namespace Shogi
 		}
 
 		public override async UniTask ExecuteAction( ShogiGame game ) {
-			int count = game.gameHistory.playedMoves.Count;
-			//Side effect. GameHistory playedmoves is modified, the last move is removed from the list
-			var lastMove = game.gameHistory.playedMoves[count - 1];
+			int playedMovesCount = game.gameHistory.playedMoves.Count;
+
+			var lastMove = game.gameHistory.playedMoves[playedMovesCount - 1];
 			await lastMove.UndoAction( game );
 
-			if(count >= 1){
-				var secondToLastMove = game.gameHistory.playedMoves[count - 2];
+			if(playedMovesCount >= 2){
+				var secondToLastMove = game.gameHistory.playedMoves[playedMovesCount - 2];
 				await secondToLastMove.UndoAction( game );
 				await secondToLastMove.ExecuteAction( game );
 			}
